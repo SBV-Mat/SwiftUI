375fc6df2879bf569d3092842c84b2716b7ac5d0
Matthias Wüst <matthias.wuest@sbv-fsa.ch>
2025-04-07 16:40:39 +0200

    xcode version

---
+++ MyWaySwiftUITests/MyWaySwiftUIUITests.swift
@@ -0,0 +1,15 @@
+//
+//  MyWaySwiftUIUITests.swift
+//  MyWaySwiftUIUITests
+//
+//  Created by Matthias Wüst on 19.03.2025.
+//
+
+import XCTest
+
+final class MyWaySwiftUIUITests: XCTestCase {
+
+    func exampleTest() throws {
+        XCTAssertEqual(2*2, 4)
+    }
+}
